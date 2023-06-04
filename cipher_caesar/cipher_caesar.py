b751cfed4e27af0aee4092fd771bd6c639f407a6
Vladislav <BarbashinVlad@mail.ru>
2023-06-04 19:00:02 +0300

    Добавил функцию расшифровывания и исправил ошибку в имени файла

--- cipher_caesar/cipher_caesar.py
+++ cipher_caesar/cipher_caesar.py
@@ -7,18 +7,18 @@ def encrypter(message, key):
         if symbol in alphabet:
             symbol_index = alphabet.find(symbol)
             index = symbol_index+key
-            if index > 25:
+            if index < 0 or index < 25:
+                result += alphabet[index]
+            else:
                 new_index = (index % 25) - 1
                 result += alphabet[new_index]
-            else:
-                result += alphabet[index]
         else:
             result += symbol
     return result
 
 
 while True:
-    line = input("Enter the message for encryption\n(Enter 'q' to exit): ").upper()
+    line = input("\nEnter the message for encryption\n(Enter 'q' to exit): ").upper()
     if line == "Q":
         print("\nGoodbye!")
         break
@@ -26,4 +26,4 @@ while True:
     step = int(input("Enter the key for encryption: "))
 
     encryption_result = encrypter(line, step)
-    print(encryption_result)
+    print(f"\nResult: {encryption_result}")
